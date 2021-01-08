10fd66b5d173540eb815156a55b39b751ae511b3
AC81555 <AC81555@IND-5CG6126QLS.ctl.intranet>
2021-01-08 12:56:54 +0530

    REQ-110 -Refactoring and null checks

--- vcs/src/vcs/DoctorService.java
+++ vcs/src/vcs/DoctorService.java
@@ -5,9 +5,16 @@ public class DoctorService {
 	
 
 	
-	public Doctor getMessage() {
-		Doctor doc=new Doctor(12,"Surabhi");
-		return doc;
+	public Doctor addDoctor(Doctor doc) {
+		Doctor doc1;
+		if(doc==null) {
+			 doc1=new Doctor(12,"Surabhi");
+			 return doc1;
+		}
+		else {
+			return doc;
+		}
+		
 	}
 	
 
