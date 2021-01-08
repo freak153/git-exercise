869eb64442a2e10fa8b14aac6c34f93dc0c13c17
AC81555 <AC81555@IND-5CG6126QLS.ctl.intranet>
2021-01-08 12:40:13 +0530

    REQ-102 -Cleaning up and adding hashCode and equals methods

--- vcs/src/vcs/Doctor.java
+++ vcs/src/vcs/Doctor.java
@@ -7,8 +7,34 @@ public class Doctor {
 	
 	
 	public Doctor() {
-		super();
 		
+		
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + doctorId;
+		result = prime * result + ((doctorName == null) ? 0 : doctorName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Doctor other = (Doctor) obj;
+		if (doctorId != other.doctorId)
+			return false;
+		if (doctorName == null) {
+			if (other.doctorName != null)
+				return false;
+		} else if (!doctorName.equals(other.doctorName))
+			return false;
+		return true;
 	}
 	public Doctor(int doctorId, String doctorName) {
 		super();
